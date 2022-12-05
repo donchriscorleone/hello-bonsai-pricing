c64cba2619bcf33d523ce9fa853d841b29dffa3a
Christopher II Lajom <christopheriilajom@gmail.com>
2022-12-05 21:59:48 +0800

    Added dropdown.

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ accordions.forEach(a => {
         a.toggleAttribute('aria-expanded');
     })
 })
+
+const dropdownButtons = document.querySelectorAll('.dropdown-arrow');
+dropdownButtons.forEach(d => {
+    d.addEventListener('click', (e) => {
+        d.toggleAttribute('aria-expanded')
+    })
+})
